6485a60cc3567e8bd210011d519a72d31f9357e5
Bhanu Kiran Chaluvadi <bkch@universal-robots.com>
2021-05-06 16:25:05 +0200

    Added test cases in main

--- impl.h
+++ impl.h
@@ -2,6 +2,7 @@
 #include <emmintrin.h>
 #include <stdio.h>
 #include <string.h>
+#include <iostream>
 
 void print128d(__m128d var)
 {
@@ -10,7 +11,13 @@ void print128d(__m128d var)
 	printf("%lf %lf\n", v64val[1], v64val[0]);
 }
 
-void sse_multiply(const double *src1, const double *src2, double *dst, int src1_w, int src1_h, int src2_w, int src2_h)
+void print_array(double *src, int size) {
+	for (int i=0; i<size; ++i){
+		std::cout << *(src+i) << std::endl;
+	}
+}
+
+void sse_multiply(double *src1, double *src2, double *dst, int src1_w, int src1_h, int src2_w, int src2_h)
 {
 	for (int x = 0; x < src1_h; x += 2)
 	{
@@ -22,21 +29,45 @@ void sse_multiply(const double *src1, const double *src2, double *dst, int src1_
 
 			for (int k = 0; k < src2_w; k += 2)
 			{
-				// __m128d I0 = _mm_loadu_pd((__m128d *)(src1 + (x + 1) * src1_w + k));
-				// __m128d I1 = _mm_loadu_pd((__m128d *)(src1 + (x + 1) * src1_w + k));
+				__m128d I0 = _mm_loadu_pd((src1 + (x + 0) * src1_w + k));
+				__m128d I1 = _mm_loadu_pd((src1 + (x + 1) * src1_w + k));
+				// print128d(I0);
+				// print128d(I1);
 
-				// __m128d I2 = _mm_set_pd(src2[(k + 1) * src2_w + y], src2[k * src2_w + y]);
-				// __m128i I3 = _mm_set_pd(src2[(k + 1) * src2_w + (y + 1)],
-				// 												src2[(k + 0) * src2_w + (y + 1)]);
+				__m128d I2 = _mm_set_pd(src2[(k + 1) * src2_w + y], src2[k * src2_w + y]);
+				__m128d I3 = _mm_set_pd(src2[(k + 1) * src2_w + (y + 1)],
+																src2[(k + 0) * src2_w + (y + 1)]);
+				// print128d(I2);
+				// print128d(I3);
 
-				// __m128d T0 = _mm_mul_pd(I0, I2);
-				// __m128d T1 = _mm_mul_pd(I0, I3);
+				__m128d T0 = _mm_mul_pd(I0, I2);
+				__m128d T1 = _mm_mul_pd(I0, I3);
+				// print128d(T0);
+				// print128d(T1);
 
-				// __m128d T2 = _mm_mul_pd(I1, I2);
-				// __m128d T3 = _mm_mul_pd(I2, I3);
+				__m128d T2 = _mm_mul_pd(I1, I2);
+				__m128d T3 = _mm_mul_pd(I1, I3);
+				// print128d(T2);
+				// print128d(T3);
 
-				// __m128d T4 = _mm_unpacklo_epi32(T0, T1);
+				__m128d T4 = _mm_unpacklo_pd(T0, T1);
+				__m128d T5 = _mm_unpackhi_pd(T0, T1);
+				// print128d(T4);
+				// print128d(T5);
+
+				T4 = _mm_add_pd(T4, T5);
+				des0 = _mm_add_pd(T4, des0);
+				// print128d(des0); // Final
+
+				T4 = _mm_unpacklo_pd(T2, T3);
+				T5 = _mm_unpackhi_pd(T2, T3);
+				T4 = _mm_add_pd(T4, T5);
+				des1 = _mm_add_pd(T4, des1);
+				// print128d(des1); // Final
 			}
+
+			_mm_store_pd((dst + ((x + 0) * src2_w) + y), des0);
+			_mm_store_pd((dst + ((x + 1) * src2_w) + y), des1);
 		}
 	}
 }
--- main.cpp
+++ main.cpp
@@ -11,7 +11,59 @@ int main()
 	print128d(d);
 
 	math::MatrixMN mat1(2, 3);
-	cout <<  "mat1 : "<< mat1 << endl;
+	cout << "mat1 : " << mat1 << endl;
 
+	// Test 1
+	double test_src1[4] = {0, 1,
+												 2, 3};
+	double test_src2[4] = {0, 1,
+												 2, 3};
+	double testout[4];
+	double expected[4] = {2, 3,
+												6, 11};
+
+	sse_multiply(test_src1, test_src1, testout, 2, 2, 2, 2);
+	std::cout << "\n Test 1 : " << std::endl;
+	print_array(testout, 4);
+
+	/*
+	// Test 2
+	double test2_src1[16] = {0, 1, 2, 3,
+													 4, 5, 6, 7,
+													 8, 9, 10, 11,
+													 12, 13, 14, 15};
+	double test2_src2[16] = {0, 1, 2, 3,
+													 4, 5, 6, 7,
+													 8, 9, 10, 11,
+													 12, 13, 14, 15};
+	double test2out[16];
+	double expected2[16] = {56, 62, 68, 74,
+												 152, 174, 196, 218,
+												 248, 286, 324, 362,
+												 344, 398, 452, 506};
+
+	sse_multiply(test2_src1, test2_src2, test2out, 4, 4, 4, 4);
+	std::cout << "\n Test 2 : " << std::endl;
+	print_array(test2out, 16);
+
+	// Test 3
+	double test3_src1[16] = {12, 27, 15, 18,
+													 45, 18, 144, 55,
+													 92, 45, 74, 9,
+													 89, 5, 99, 127};
+	double test3_src2[8] = {-12, 3,
+													1.5, -5,
+													4, 0,
+													125.5, 2.7};
+	double test3out[8];
+	double expected3[8] = {2215.5, -50.4,
+												6965.5, 193.5,
+												389, 75.3,
+												15274, 584.9};
+
+	sse_multiply(test3_src1, test3_src2, test3out, 4, 4, 4, 2);
+	std::cout << "\n Test 3 : " << std::endl;
+	print_array(test3out, 8);
+	*/
 	return 0;
 }
