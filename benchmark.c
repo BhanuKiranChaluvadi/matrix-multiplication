25601f95f74be72a0d7d876f15292610c0bcab1a
Bhanu Kiran Chaluvadi <bkch@universal-robots.com>
2021-05-07 11:45:43 +0200

    formatting

--- benchmark.c
+++ benchmark.c
@@ -16,83 +16,83 @@
 
 static float diff_in_us(struct timespec t1, struct timespec t2)
 {
-    struct timespec diff;
-    if (t2.tv_nsec - t1.tv_nsec < 0)
-    {
-        diff.tv_sec = t2.tv_sec - t1.tv_sec - 1;
-        diff.tv_nsec = t2.tv_nsec - t1.tv_nsec + 1000000000;
-    }
-    else
-    {
-        diff.tv_sec = t2.tv_sec - t1.tv_sec;
-        diff.tv_nsec = t2.tv_nsec - t1.tv_nsec;
-    }
-    return (diff.tv_sec * 1000000.0 + diff.tv_nsec / 1000.0);
+	struct timespec diff;
+	if (t2.tv_nsec - t1.tv_nsec < 0)
+	{
+		diff.tv_sec = t2.tv_sec - t1.tv_sec - 1;
+		diff.tv_nsec = t2.tv_nsec - t1.tv_nsec + 1000000000;
+	}
+	else
+	{
+		diff.tv_sec = t2.tv_sec - t1.tv_sec;
+		diff.tv_nsec = t2.tv_nsec - t1.tv_nsec;
+	}
+	return (diff.tv_sec * 1000000.0 + diff.tv_nsec / 1000.0);
 }
 
 int main(int argc, char *argv[])
 {
-    unsigned int max_size = 512;
-    unsigned int loop_cout = 1000;
+	unsigned int max_size = 512;
+	unsigned int loop_cout = 1000;
 
-    for (unsigned int size = 2; size <= max_size; size += 2)
-    {
-        printf("size %d\n", size);
-        float naive_total_elapsed_time = 0;
-        float sse_total_elapsed_time = 0;
+	for (unsigned int size = 2; size <= max_size; size += 2)
+	{
+		printf("size %d\n", size);
+		float naive_total_elapsed_time = 0;
+		float sse_total_elapsed_time = 0;
 
-        for (unsigned int i = 0; i <= loop_cout; i++)
-        {
-            // square matrix
-            unsigned int TEST_H = size;
-            unsigned int TEST_W = size;
+		for (unsigned int i = 0; i <= loop_cout; i++)
+		{
+			// square matrix
+			unsigned int TEST_H = size;
+			unsigned int TEST_W = size;
 
-            struct timespec start, end;
-            double *src1 = (double *)memalign(64, sizeof(double) * TEST_W * TEST_H);
-            double *src2 = (double *)memalign(64, sizeof(double) * TEST_W * TEST_H);
-            double *dst = (double *)memalign(64, sizeof(double) * TEST_W * TEST_H);
-            double *correct = (double *)memalign(64, sizeof(double) * TEST_W * TEST_H);
+			struct timespec start, end;
+			double *src1 = (double *)memalign(64, sizeof(double) * TEST_W * TEST_H);
+			double *src2 = (double *)memalign(64, sizeof(double) * TEST_W * TEST_H);
+			double *dst = (double *)memalign(64, sizeof(double) * TEST_W * TEST_H);
+			double *correct = (double *)memalign(64, sizeof(double) * TEST_W * TEST_H);
 
-            srand(time(NULL));
-            for (int i = 0; i < TEST_H; ++i)
-            {
-                for (int j = 0; j < TEST_W; ++j)
-                {
-                    src1[i * TEST_W + j] = rand();
-                    src2[i * TEST_W + j] = rand();
-                }
-            }
+			srand(time(NULL));
+			for (int i = 0; i < TEST_H; ++i)
+			{
+				for (int j = 0; j < TEST_W; ++j)
+				{
+					src1[i * TEST_W + j] = rand();
+					src2[i * TEST_W + j] = rand();
+				}
+			}
 
-            clock_gettime(CLOCK_REALTIME, &start);
-            naive_multiply(src1, src2, correct, TEST_W, TEST_H, TEST_W, TEST_H);
-            clock_gettime(CLOCK_REALTIME, &end);
-            naive_total_elapsed_time += diff_in_us(start, end);
+			clock_gettime(CLOCK_REALTIME, &start);
+			naive_multiply(src1, src2, correct, TEST_W, TEST_H, TEST_W, TEST_H);
+			clock_gettime(CLOCK_REALTIME, &end);
+			naive_total_elapsed_time += diff_in_us(start, end);
 
-            clock_gettime(CLOCK_REALTIME, &start);
-            sse_multiply(src1, src2, dst, TEST_W, TEST_H, TEST_W, TEST_H);
-            clock_gettime(CLOCK_REALTIME, &end);
-            sse_total_elapsed_time += diff_in_us(start, end);
+			clock_gettime(CLOCK_REALTIME, &start);
+			sse_multiply(src1, src2, dst, TEST_W, TEST_H, TEST_W, TEST_H);
+			clock_gettime(CLOCK_REALTIME, &end);
+			sse_total_elapsed_time += diff_in_us(start, end);
 
-            // if (0 != memcmp(correct, dst, sizeof(double) * TEST_W * TEST_H))
-            // {
-            // printf("naive : ");
-            // print_array(correct, size * size);
-            // printf("sse : ");
-            // print_array(correct, size * size);
-            // }
-            // assert(0 == memcmp(correct, dst, sizeof(double) * TEST_W * TEST_H) &&
-            //    "Verification fails");
-            // std::this_thread::sleep_for(std::chrono::milliseconds(100));
+			// if (0 != memcmp(correct, dst, sizeof(double) * TEST_W * TEST_H))
+			// {
+			// printf("naive : ");
+			// print_array(correct, size * size);
+			// printf("sse : ");
+			// print_array(correct, size * size);
+			// }
+			// assert(0 == memcmp(correct, dst, sizeof(double) * TEST_W * TEST_H) &&
+			//    "Verification fails");
+			// std::this_thread::sleep_for(std::chrono::milliseconds(100));
 
-            delete src1;
-            delete src2;
-            delete dst;
-            delete correct;
-        }
-        float avg_native_elapsed_time = naive_total_elapsed_time / float(loop_cout);
-        float avg_sse_elapsed_time = sse_total_elapsed_time / float(loop_cout);
-        printf("naive  %lf\n", avg_native_elapsed_time);
-        printf("sse  %lf\n", avg_sse_elapsed_time);
-    }
-    return 0;
+			delete src1;
+			delete src2;
+			delete dst;
+			delete correct;
+		}
+		float avg_native_elapsed_time = naive_total_elapsed_time / float(loop_cout);
+		float avg_sse_elapsed_time = sse_total_elapsed_time / float(loop_cout);
+		printf("naive  %lf\n", avg_native_elapsed_time);
+		printf("sse  %lf\n", avg_sse_elapsed_time);
+	}
+	return 0;
 }
--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
-// g++ -O2 -msse3 MatrixMN.cpp main.cpp
+// g++ -O2 -msse3 main.cpp
 #include "impl.h"
-#include "MatrixMN.h"
 #include <iostream>
 
 using namespace std;
@@ -22,17 +21,17 @@ int main()
 
 	// Test 2 : Negative
 	double test2_src1[4] = {0, -1,
-												 2, 3};
+													2, 3};
 	double test2_src2[4] = {0, 1,
-												 2, -3};
+													2, -3};
 	double test2out[4];
 	double expected2[4] = {-2, 3,
-												6, -7};
+												 6, -7};
 
 	sse_multiply(test2_src1, test2_src2, test2out, 2, 2, 2, 2);
 	std::cout << "\n Test 2 : " << std::endl;
 	print_array(test2out, 4);
-	
+
 	// Test 3
 	double test3_src1[16] = {0, 1, 2, 3,
 													 4, 5, 6, 7,
@@ -44,15 +43,15 @@ int main()
 													 12, 13, 14, 15};
 	double test3out[16];
 	double expected3[16] = {56, 62, 68, 74,
-												 152, 174, 196, 218,
-												 248, 286, 324, 362,
-												 344, 398, 452, 506};
+													152, 174, 196, 218,
+													248, 286, 324, 362,
+													344, 398, 452, 506};
 
 	sse_multiply(test3_src1, test3_src2, test3out, 4, 4, 4, 4);
 	std::cout << "\n Test 3 : " << std::endl;
 	print_array(test2out, 16);
-	
-		double test4_src1[16] = {0, 1, 2, 3,
+
+	double test4_src1[16] = {0, 1, 2, 3,
 													 4, 5, 6, 7,
 													 8, 9, 10, 11,
 													 12, 13, 14, 15};
@@ -81,13 +80,13 @@ int main()
 													125.5, 2.7};
 	double test5out[8];
 	double expected5[8] = {2215.5, -50.4,
-												6965.5, 193.5,
-												389, 75.3,
-												15274, 584.9};
+												 6965.5, 193.5,
+												 389, 75.3,
+												 15274, 584.9};
 
 	sse_multiply(test5_src1, test5_src2, test5out, 4, 4, 2, 4);
 	std::cout << "\n Test 5 : " << std::endl;
 	print_array(test5out, 8);
-	
+
 	return 0;
 }
